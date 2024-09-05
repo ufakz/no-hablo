42f73f049da409f72fcd003a9195d506eac0dd8c
Farouq Umar <ufakz99@gmail.com>
2024-09-05 04:01:41 +0200

    fix: styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
